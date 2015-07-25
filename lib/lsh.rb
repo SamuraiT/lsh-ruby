9582c01969e6448f49c43b6555590cc68cd8f65b
Matsu <Matsu@Matsus-MacBook-Air.local>
2015-07-25 17:24:59 +0900

    Add hash and hamming distance

--- lib/lsh.rb
+++ lib/lsh.rb
@@ -25,5 +25,24 @@ module Lsh
         @random_indexes
       end
     end
+
+    def hash(unaried_query)
+      make_random_indexes(unaried_query.length) unless random_indexes
+      random_indexes.map{ |i| unaried_query[i] }
+    end
+
+    def hamming_distance(p,q)
+      distance = 0
+      xor(p,q).each_char do |bit|
+        distance += 1 if bit == "1"
+      end
+      distance
+    end
+
+    def xor(p,q)
+      p = p.join('').to_i(2)
+      q = q.join('').to_i(2)
+      (p ^ q).to_s(2)
+    end
   end
 end
--- spec/lsh_spec.rb
+++ spec/lsh_spec.rb
@@ -27,4 +27,30 @@ describe Lsh do
       expect(Lsh::make_random_indexes(Lsh::unary(query).length).length).to eq(k)
     end
   end
+
+  describe "Lsh#hash" do
+    context "when c=4, q=[1, 2, 4], random-indexes=[2,4,8,11]" do
+      it do
+        c = 4
+        q=[1, 2, 4]
+        Lsh::random_indexes = [2,4,8,11]
+        expect(Lsh::hash(Lsh::unary(q, c))).to eq([0,1,1,1])
+      end
+    end
+  end
+
+  describe "Lsh#hamming_distance" do
+    context "when p=[1,0,3], q=[2,0,2] c=3, k=4, random-indexes=[2,4,8,11]" do
+      it  do
+        Lsh::c = 3
+        Lsh::k = 4
+        p = [1,0,3]
+        q = [2,0,2]
+        Lsh::random_indexes = [2,4,8,11]
+        p = Lsh::unary(p, Lsh::c)
+        q = Lsh::unary(q, Lsh::c)
+        expect(Lsh::hamming_distance(p,q)).to eq(2)
+      end
+    end
+  end
 end
