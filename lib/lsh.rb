4a55cb5a17028b1fbe7d587446269efcc5ee864f
Matsu <Matsu@Matsus-MacBook-Air.local>
2015-07-25 16:31:32 +0900

    Add random indexes which create random indexes

--- lib/lsh.rb
+++ lib/lsh.rb
@@ -2,14 +2,27 @@ require "lsh/version"
 
 module Lsh
   class << self
+    @@k = 5
+    def k=(k)
+      @@k = k
+    end
 
-    def unary(q, c)
+    def unary(query, c=4)
       _unary = []
-      q.each do |i|
+      query.each do |i|
         i.times { _unary << 1 }
         (c - i).times { _unary << 0 }
       end
       _unary
+    end
+
+    def random_index(length)
+      @@random_index ||= begin
+        @@random_index = []
+        indexs = [*0...length]
+        @@k.times { @@random_index << indexs.shuffle!.pop }
+        @@random_index
+      end
     end
   end
 end
--- spec/lsh_spec.rb
+++ spec/lsh_spec.rb
@@ -1,15 +1,30 @@
 require 'spec_helper'
 
 describe Lsh do
+  let(:query) { [1, 2, 4] }
+  let(:c) { 4 }
+  let(:k) { 5 }
+
   it 'has a version number' do
     expect(Lsh::VERSION).not_to be nil
   end
 
   describe 'Lsh#unary' do
     it "turns query as binary vector" do
-      q = [1, 2, 4]
-      c = 4
-      expect(Lsh::unary(q, c)).to eq([1,0,0,0,1,1,0,0,1,1,1,1])
+      expect(Lsh::unary(query, c)).to eq([1,0,0,0,1,1,0,0,1,1,1,1])
+    end
+  end
+
+  describe 'Lsh#random_index' do
+    it do
+      Lsh::k = k
+      indexs = Lsh::random_index(Lsh::unary(query).length)
+      expect(indexs).to eq(indexs)
+    end
+
+    it "returns random index"do
+      Lsh::k = k
+      expect(Lsh::random_index(Lsh::unary(query).length).length).to eq(k)
     end
   end
 end
